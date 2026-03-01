5f1ccdf85e1fa9dbbd6ca8d75272f2626b004941
ArmageddonSR <151889707+ArmageddonSR@users.noreply.github.com>
2026-03-01 05:04:42 +0000

    thing

--- Stack.C
+++ Stack.C
@@ -1,15 +1,21 @@
 #include<stdio.h>
+#include<stdlib.h>  
 
 struct Node
 {
-    int key;
-    struct Node *node;
-}
+    int data;
+    struct Node *nextptr;
+};
+typedef struct Node node;
+typedef struct Node* Nodeptr;
+Nodeptr top = NULL;
 
 int main(int argc, char* argv[])
-{
-     struct Node *root;
-     Node root = NULL;
-
+{   
      return 0;
+}
+
+void Push(Nodeptr *top, int x)
+{
+    node* newNode = (Nodeptr)malloc((sizeof(node)));
 }
